495093133ba74ea2005f255a4324673d676d90dd
P Anjali <P Anjali@192.168.0.104>
2020-03-03 20:44:08 +0530

    Created a New Maven Project

---
+++ Exception_Loggers/src/main/java/com/epam/Exception_Loggers/Main.java
@@ -0,0 +1,12 @@
+package com.epam.Exception_Loggers;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		SI_CI_Interest.InterestCalculation();
+		Construction_cost.Cost();
+	}
+	
+	
+
+}
